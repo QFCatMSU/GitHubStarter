f5f884600332d76bf6aae07d4b085130ce53f567
Charlie <belinsky@msu.edue>
2021-06-04 14:21:31 -0400

    stjrtjytj

--- scripts/test2.r
+++ scripts/test2.r
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-sadgaedrghd
-=======
-fsafsdasadf
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> a028b4a6378507bf1b3a51e8a2a780e806295d39
-=======
-fds
->>>>>>> 3bf47b546fd40a3583fae510bf5c4817bf21d9d1
-=======
-fdscxvf
->>>>>>> c1628d8c2d42101fad805813d9363b892b59a867
+hersdhsh
