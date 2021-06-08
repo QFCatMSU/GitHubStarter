82ce9324c2e6095a271a4e024e216fc7162dc149
Charlie <belinsky@msu.edue>
2021-06-08 15:19:50 -0400

    rtyuertyre

--- scripts/test2.r
+++ scripts/test2.r
@@ -1,5 +1,10 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 fdscxvgdfdsfgdsgfretg
 =======
 hersdhsh
 >>>>>>> f5f884600332d76bf6aae07d4b085130ce53f567
+=======
+222
+333
+>>>>>>> 4bea68b7b82633f925f769fbf0899be1805dc716
