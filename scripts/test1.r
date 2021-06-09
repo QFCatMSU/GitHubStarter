ec43e4e8caff14e7b746b5f5e6d6495fd7582cb9
Charlie <belinsky@msu.edue>
2021-06-09 11:32:24 -0400

    dfgjd

--- scripts/test1.r
+++ scripts/test1.r
@@ -1,11 +1,11 @@
 <<<<<<< HEAD
-<<<<<<< HEAD
-catsdfsffds(12)
+<<<<<< HEAD
+catsdfsffs(12)
 =======
 cat(1)
 112
 >>>>>>> ba13ace222956c43c6b07ec72655a9c5194b1db6
-=======
+=======kjl;
 111
 444
->>>>>>> 4bea68b7b82633f925f769fbf0899be1805dc716
+>>>>>>> 4bea68bb82633f925f769fbf0899be1805dc716
--- scripts/test2.r
+++ scripts/test2.r
@@ -1,10 +1,7 @@
 <<<<<<< HEAD
-<<<<<<< HEAD
+<<<<<< HEAD
 fdscxvgdfdsfgdsgfretg
-=======
-hersdhsh
->>>>>>> f5f884600332d76bf6aae07d4b085130ce53f567
-=======
+=======hersd>> f5f884600332d76bf6aae07d4b085130ce53f567
+=======jhkg
 222
-333
->>>>>>> 4bea68b7b82633f925f769fbf0899be1805dc716
+333>>>>>hjkghk
